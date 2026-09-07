acf7466874f7daac74baca6dbd94dac6e9246510
oraclemoss <vaughneric4@gmail.com>
2026-09-07 17:09:02 -0500

    import Cell class into grid.js

--- src/grid.js
+++ src/grid.js
@@ -1,15 +1,17 @@
+import { Cell } from "./cell.js";
+
 const FRAME_RATE = 8; // Frames per second
 
 export class Grid {
-    // Constructor
-    constructor(rows = 0, cols = 0) {
-        this.rows = rows;
-        this.cols = cols;
-        this.matrix = Array.from({ length: rows }, () => Array(cols).fill(0));
-    }
+  // Constructor
+  constructor(rows = 0, cols = 0) {
+    this.rows = rows;
+    this.cols = cols;
+    this.matrix = Array.from({ length: rows }, () => Array(cols).fill(0));
+  }
 
-    // Draws the grid
-    draw() {
-        // TODO 
-    }
+  // Draws the grid
+  draw() {
+    // TODO
+  }
 }
