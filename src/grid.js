90117399ad671d756119d850a71dd27c334c0dfc
oraclemoss <vaughneric4@gmail.com>
2026-09-07 18:05:18 -0500

    add access to <canvas> to draw the Grid in Grid.js

--- src/grid.js
+++ src/grid.js
@@ -1,5 +1,8 @@
 import { Cell } from "./Cell.js";
 
+const canvas = document.getElementById("gameOfLifeCanvas");
+const ctx = canvas.getContext("2d");
+
 const FRAME_RATE = 8; // Frames per second
 
 export class Grid {
@@ -7,8 +10,10 @@ export class Grid {
   constructor(rows = 10, cols = 10) {
     this.rows = rows;
     this.cols = cols;
-    this.matrix = Array.from({ length: rows }, (_, r) => 
-                  Array.from({ length: cols }, (_, c) => new Cell(r, c)) );
+    // Populate our matrix with cells
+    this.matrix = Array.from({ length: rows }, (_, r) =>
+      Array.from({ length: cols }, (_, c) => new Cell(r, c)),
+    );
   }
 
   // Draws the grid
