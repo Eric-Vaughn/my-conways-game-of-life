ef028c44fae52d329b53dbcea9ea8a12cfdf44cf
oraclemoss <vaughneric4@gmail.com>
2026-09-07 17:30:28 -0500

    add const value for a for Cell.js width
    Height is unnecessary since Cells are square

--- src/cell.js
+++ src/cell.js
@@ -1,8 +1,10 @@
+const CELL_WIDTH = 50; // Cells are square, so width is used for height as well
+
 export class Cell {
   constructor(row, col, value = 0) {
     this.row = row;
     this.col = col;
     this.value = value;
-    this.isAlive = false; // Example for Game of Life
+    this.isAlive = false;
   }
 }
