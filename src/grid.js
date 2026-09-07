266433c087b8452ac6610d9d57a76f8f280493cf
oraclemoss <vaughneric4@gmail.com>
2026-09-07 16:10:19 -0500

    create grid.js constructor

---
+++ src/grid.js
@@ -0,0 +1,9 @@
+const FRAME_RATE = 8; // Frames per second
+
+export class Grid {
+    // Constructor
+    constructor(rows = 0, cols = 0) {
+        this.rows = rows;
+        this.cols = cols;
+    }
+}
