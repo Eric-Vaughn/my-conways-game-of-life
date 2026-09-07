f17e1f7a4f41ce3a538ab1d707dfa2126e89e276
oraclemoss <vaughneric4@gmail.com>
2026-09-07 18:10:46 -0500

    fix unused parameter in Cell class constructor

--- src/cell.js
+++ src/cell.js
@@ -2,7 +2,7 @@ const CELL_WIDTH = 50; // In pixels
 const CELL_PADDING = 2; // in pixels
 
 export class Cell {
-  constructor(row, col, value = 0) {
+  constructor(row, col) {
     this.row = row;
     this.col = col;
     this.isAlive = false;
